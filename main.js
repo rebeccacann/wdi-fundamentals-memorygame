d9cb17aaedf3e559962e3c49c7e609fa335af719
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-03 22:30:59 -0400

    js beginnings

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// GitHub version
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
 
