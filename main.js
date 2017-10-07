24f4a781a65ae3057d53b379e22cadf00c9f32c8
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-06 21:28:47 -0400

    added var cardTwo

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // GitHub version
 console.log("Up and running!");
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
+
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
