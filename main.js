15abfc9e6d43712e6a15d81517101641f6527ef3
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-06 21:47:36 -0400

    conditional: do the cards match?
    if... else conditional created with cardsInPlay array

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
 
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again");
+    }
+}
+
