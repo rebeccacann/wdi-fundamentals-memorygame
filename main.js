99752ffda52bbe3ad1bae62a2992ad6913f554d4
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-06 21:14:39 -0400

    added element to array
    added var cardOne to the empty array cardsInPlay so now it's not empty

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ console.log("Up and running!");
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
 
