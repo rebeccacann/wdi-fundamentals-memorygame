020cdeb0451a7133238caac7714222fc21248e3d
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-08 15:15:03 -0400

    added var checkForMatch
    and put the conditional with the two alert options inside of the checkForMatch function

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 cardsInPlay.push(cards[0]);
 
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    if (cardsInPlay.length === 2) {
+ var checkForMatch = function() {
         if (cardsInPlay[0] === cardsInPlay[1]) {
             alert("You found a match!");
         } else {
             alert("Sorry, try again");
         }
-    }
+ }
+ 
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    if (cardsInPlay.length === 2) 
 }
 flipCard(2);
 flipCard(3);
