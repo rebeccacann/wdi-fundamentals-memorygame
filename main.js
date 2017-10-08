f5856b2c4b9841cd02335e54edd074fdc40700ea
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-08 19:58:14 -0400

    code cleanup
    very confusing

--- main.js
+++ main.js
@@ -12,11 +12,13 @@ cardsInPlay.push(cards[0]);
             alert("Sorry, try again");
         }
  }
- 
+
 var flipCard = function(cardId) {
     console.log("User flipped " + cards[cardId]);
-    if (cardsInPlay.length === 2) 
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
 }
+flipCard(0);
 flipCard(2);
-flipCard(3);
 
